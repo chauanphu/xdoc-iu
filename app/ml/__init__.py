c1b5215263bbba0bdd9f5803dbe1f2ef0e6a6539
chauanphu <chauanphu@gmail.com>
2025-04-07 13:18:15 +0000

    deploy and install new dependencies

--- app/ml/__init__.py
+++ app/ml/__init__.py
@@ -3,7 +3,10 @@ from .model import DiabetesPredictor, CardioPredictor
 def get_predictor(disease: str):
     try:
         if disease.lower() == "diabetes":
-            return DiabetesPredictor(model_path="pretrained/diabetes_model.json")
+            return DiabetesPredictor(
+                model_path="pretrained/diabetes_model.json",
+                scaler_path="pretrained/diabetes.scaler.pkl",
+            )
         elif disease.lower() == "cardiovascular":
             return CardioPredictor(model_path="pretrained/cardio_model.json")
         else:
--- app/ml/model.py
+++ app/ml/model.py
@@ -2,6 +2,10 @@
 import xgboost as xgb
 import numpy as np
 from abc import ABC, abstractmethod
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import joblib
+import os
 
 class DiseasePredictor(ABC):
     @abstractmethod
@@ -17,18 +21,22 @@ class DiseasePredictor(ABC):
         pass
 
 class DiabetesPredictor(DiseasePredictor):
-    def __init__(self, model_path: str):
-        self.model = xgb.Booster()
-        self.model.load_model(model_path)
-    
+    def __init__(self, model_path: str, scaler_path: str = None):
+        self.model = xgb.XGBClassifier()
+        self.features = ['AGE', 'Urea', 'Cr', 'HbA1c', 'Chol', 'TG', 'HDL', 'LDL', 'VLDL', 'BMI']
+        self.scaler = StandardScaler()
+        if model_path:
+            self.load(model_path, scaler_path)
+        
     def preprocess(self, data: dict) -> np.ndarray:
-        # Extract diabetes-specific features
-        features = np.array([data[k] for k in sorted(data.keys()) if k in ["glucose", "bmi", "age"]])
-        return features.reshape(1, -1)
+        # Convert to dataframe
+        df = pd.DataFrame([data])
+        df = df[self.features]
+        features_scaled = self.scaler.transform(df)
+        return features_scaled
 
     def predict(self, preprocessed_data: np.ndarray) -> dict:
-        dmatrix = xgb.DMatrix(preprocessed_data)
-        preds = self.model.predict(dmatrix)
+        preds = self.model.predict_proba(preprocessed_data)
         return {"raw_prediction": preds}
 
     def postprocess(self, raw_prediction: dict) -> dict:
@@ -37,6 +45,15 @@ class DiabetesPredictor(DiseasePredictor):
         prediction = int(preds.argmax())
         return {"prediction": prediction, "confidence": confidence}
     
+    def load(self, model_path: str, scaler_path: str = None):
+        """Load model and scaler"""
+        # Load the model
+        self.model.load_model(model_path)
+        
+        # Load the scaler if path is provided and file exists
+        if scaler_path and os.path.exists(scaler_path):
+            self.scaler = joblib.load(scaler_path)
+
 class CardioPredictor(DiseasePredictor):
     def __init__(self, model_path: str):
         self.model = xgb.Booster()
